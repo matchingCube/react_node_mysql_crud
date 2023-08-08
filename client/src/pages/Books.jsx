7a7911151f16710a1c2ccea1afd0326af0b55ca4
matchingCube <matchingCube@outlook.com>
2023-08-08 13:54:16 +0200

    get data by calling api in frontend

--- client/src/pages/Books.jsx
+++ client/src/pages/Books.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function Books() {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    const fetchAllBooks = async () => {
+      try {
+        const res = await axios.get("http://localhost:8800/books");
+        setBooks(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchAllBooks();
+  }, []);
   return (
-    <div>Books</div>
-  )
+    <div>
+      <h1 style={{ textAlign: "center" }}>Book Shop</h1>
+      <div className="books">
+        {books.map((book, idx) => (
+          <div className="book" key={`book_${idx}`}>
+            {book.cover && <img className="bookImg" src={book.cover} alt={book.title} />}
+            <h2>{book.title}</h2>
+            <p>{book.desc}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Books
+export default Books;
