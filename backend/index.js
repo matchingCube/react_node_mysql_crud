43ca585ec30cf1554b5013a3e9afb51ff98e674f
matchingCube <matchingCube@outlook.com>
2023-08-08 03:34:51 +0200

    init client react app

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,39 @@
-import express from 'express'
+import express from "express";
+import mysql from "mysql";
 
-const app = express()
+const app = express();
 
-app.listen(8800, ()=>{
-  console.log('Connected to backend!')
-})
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "test",
+});
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json("Hello. This is the backend.");
+});
+
+app.get("/books", (req, res) => {
+  const q = "SELECT * FROM books";
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
+
+app.post("/books", (req, res) => {
+  const q = "INSERT INTO books (`title`, `desc`, `cover`) VALUES (?)";
+  const values = [req.body.title, req.body.desc, req.body.cover];
+
+  db.query(q, [values], (err, data) => {
+    if (err) return res.json(err);
+    return res.json("Book has been added successfully");
+  });
+});
+
+app.listen(8800, () => {
+  console.log("Connected to backend!");
+});
